a05a1505dbb76c8222f70e39fe4aafe6c414ef38
Jeremy Rieunier <jeremy.rieunier@gmail.com>
2025-02-21 15:40:15 +0000

    Create fact table containing trip data

--- 04-analytics-engineering/models/core/dim_zones.sql
+++ 04-analytics-engineering/models/core/dim_zones.sql
@@ -10,4 +10,4 @@ select
     zone,
     -- Replace Boro Zone by Green Zone since it's only served by Green Taxis
     replace(service_zone, "Boro Zone", "Green Zone") as service_zone
-from {{ ref("taxi_zone_lookup") }}
+from {{ ref('taxi_zone_lookup') }}
